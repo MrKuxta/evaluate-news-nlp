8d8469a712dd13e37a63699ed7794408920a7c59
Stavros Koukourikos <mrkuxta@gmail.com>
2021-08-24 00:48:48 +0100

    adding styles2

--- src/client/index.js
+++ src/client/index.js
@@ -3,11 +3,13 @@ import { handleSubmit } from './js/formHandler'
 
 //console.log(checkForName);
 
+/*
 import "./styles/base.scss"
 import "./styles/footer.scss"
 import "./styles/form.scss"
 import "./styles/header.scss"
 import "./styles/resets.scss"
+*/
 
 alert("I EXIST")
 console.log("CHANGE!!");
